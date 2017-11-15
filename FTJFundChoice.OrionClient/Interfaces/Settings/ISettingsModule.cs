0de451a5b96def292a75e28974273cb0487651fd
Brandon Roberts <brandonlroberts@outlook.com>
2017-11-15 16:18:02 -0500

    Checked in update to ISettingsModule parameters.

--- FTJFundChoice.OrionClient/Interfaces/Settings/ISettingsModule.cs
+++ FTJFundChoice.OrionClient/Interfaces/Settings/ISettingsModule.cs
@@ -10,7 +10,7 @@ namespace FTJFundChoice.OrionClient.Interfaces.Settings
 {
     public interface ISettingsModule
     {
-        Task<IResult<Logo>> UploadMainThemeLogo(string entityType, long entityId,string brokerDealerLogo);
+        Task<IResult<Logo>> UploadMainThemeLogo(string entityType, long entityId,string logoData);
 
         Task<IResult<Logo>> UploadAdvisorImage(string entityType, long entityId, string logoData);
     }
